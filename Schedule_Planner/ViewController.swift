f5640bd019d9dfb4cb75d9d6c23c6ebf59c4c8b7
raam54 <raam54@icloud.com>
2020-09-20 15:11:12 +0300

    Initial Commit

---
+++ Schedule_Planner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Schedule_Planner
+//
+//  Created by Nick on 20.09.2020.
+//  Copyright © 2020 Nick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
